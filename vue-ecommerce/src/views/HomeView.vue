46c857bcb31cb311075fe1cf13f2566b32acf65f
muhammad salman <111515109+Muhammadsalman53@users.noreply.github.com>
2023-08-23 17:34:37 +0500

    Product-detail page display data through api

--- vue-ecommerce/src/views/HomeView.vue
+++ vue-ecommerce/src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+// import ProductDetail from '../components/Product-Detail.vue';
+// import BuyNow from '../components/BuyNow.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <!-- <h1>hello</h1> -->
+    <!-- <ProductDetail/> -->
+    <!-- <BuyNow/> -->
   </main>
 </template>
