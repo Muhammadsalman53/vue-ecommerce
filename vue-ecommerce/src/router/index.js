c620b564564907c094193e2c1748eef4a9d57206
muhammad salman <111515109+Muhammadsalman53@users.noreply.github.com>
2023-08-24 16:32:41 +0500

    Dynamic data

--- vue-ecommerce/src/router/index.js
+++ vue-ecommerce/src/router/index.js
@@ -1,23 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import ProductDetail from '../components/Product-Detail.vue';
+import BuyNow from '../components/BuyNow.vue'
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      // component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+const routes =[
+
+{
+  path: '/product/:id',
+ name: 'ProductDetail',
+component: ProductDetail
+},
+{
+  path: '/buynow',
+ name: 'BuyNow',
+component: BuyNow
+}
+
+];
+
+const router=createRouter({
+  history:createWebHistory(),
+  routes
+});
 
 export default router
