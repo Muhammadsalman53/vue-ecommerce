7dba0189bc41193643f35bef813b5cafe807dc0a
muhammmadaj12 <buttgreat543@gmail.com>
2023-08-24 16:42:58 +0500

    This is api with responvie Landing Page

--- vue-ecommerce/src/App.vue
+++ vue-ecommerce/src/App.vue
@@ -1,7 +1,8 @@
 <script setup>
+import { RouterView } from 'vue-router';
 import Header from './components/Header.vue';
 import Footer from './components/Footer.vue';
-import { RouterView } from 'vue-router';
+
 </script>
 
 <template>
--- vue-ecommerce/src/main.js
+++ vue-ecommerce/src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router/index';;
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min';
 import './assets/main.css';
 
 createApp(App).use(router).mount('#app');
