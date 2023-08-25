790a4178148ef4a8bd7b1148bef573db083cd9b3
Mz-Bug <111413327+Mz-Bug@users.noreply.github.com>
2023-08-25 04:52:39 -0700

    Category

--- vue-ecommerce/src/components/Mobileprod.vue
+++ vue-ecommerce/src/components/Mobileprod.vue
@@ -3,34 +3,53 @@
   <section class="pt-4" id="mobileproduct">
     <div class="container">
       <div class="row" id="mob-head">
-          <div class="col-lg-4 mt-3 col-md-12 col-sm-12" id="category-head">
-            <h3>All Categories Products:</h3>
-          </div>
-          <div class="col-lg-7 my-3 col-md-12 col-sm-12" id="category-btns">
-            <select class="form-select form-select-md" aria-label="Default select categories">
-              <option selected>All categories</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-            </select>
-            <form class="d-flex">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-md btn-outline-success" type="submit">Search</button>
-            </form>
-            <button class="btn btn-md btn-outline-primary" @click="$router.push('/productcategory')" type="button">View All ></button>
-          </div>
+        <div class="col-lg-4 mt-3 col-md-12 col-sm-12" id="category-head">
+          <h3>All Categories Products:</h3>
+        </div>
+        <div class="col-lg-7 my-3 col-md-12 col-sm-12" id="category-btns">
+          <select
+            class="form-select form-select-md"
+            aria-label="Default select categories"
+            v-model="selectedCategory"
+          >
+            <option disabled value="">Select a Category</option>
+            <option
+              v-for="category in categories"
+              :key="category"
+              :value="category"
+            >
+              {{ category }}
+            </option>
+          </select>
+        </div>
       </div>
       <div class="row" id="cards-wrapp">
-        <div class="mb-4" v-for="(item, index) in list" :key="item.id" id="landing-cards" >
-          <div class="card" @click="item_detail(item)" style=" min-height: 130px;">
-            <img :src="item.images[0]" style="height: 250px;" class="card-img-top p-4" alt="...">
-            <hr>
+        <div
+          class="mb-4"
+          v-for="(item, index) in list"
+          :key="item.id"
+          id="landing-cards"
+        >
+          <div
+            class="card"
+            @click="item_detail(item)"
+            style="min-height: 130px"
+          >
+            <img
+              :src="item.images[0]"
+              style="height: 250px"
+              class="card-img-top p-4"
+              alt="..."
+            />
+            <hr />
             <div class="card-body">
               <h5 class="card-title">{{ item.title }}</h5>
-              <p style="min-height: 100px;" class="card-text">{{ item.description }}</p>
-              <div class="card-footer ">
+              <p style="min-height: 100px" class="card-text">
+                {{ item.description }}
+              </p>
+              <div class="card-footer">
                 <span class="text-start pe-auto">Price:</span>
-                <span class="fw-bold float-end">${{ item.price }}</span>  
+                <span class="fw-bold float-end">${{ item.price }}</span>
               </div>
             </div>
           </div>
@@ -41,21 +60,52 @@
 </template>
 
 <script>
-import axios from 'axios';
-import { onMounted, ref } from 'vue';
-import { useRouter } from "vue-router"
+import axios from "axios";
+import { onMounted, ref, watch } from "vue";
+import { useRouter } from "vue-router";
 
 export default {
-  name: 'Mobileprod',
+  name: "Mobileprod",
   setup() {
     const list = ref([]);
     const route = useRouter();
-
+    const categories = ref([]);
+    const selectedCategory = ref("");
     function item_detail(item) {
       console.log(item);
-      route.push({ name: 'product_detail', path: '/product_detail', params: { id: item.id } });
+      route.push({
+        name: "product_detail",
+        path: "/product_detail",
+        params: { id: item.id },
+      });
     }
-
+    const fetchCategories = () => {
+      axios
+        .get("https://dummyjson.com/products/categories")
+        .then((response) => {
+          console.log("Categories:", response.data);
+          categories.value = response.data;
+        })
+        .catch((error) => {
+          console.error("Error fetching categories:", error);
+          categories.value = [{ id: "", name: "Error loading categories" }];
+        });
+    };
+    const fetchProducts = () => {
+      if (selectedCategory.value) {
+        axios
+          .get(
+            `https://dummyjson.com/products/category/${selectedCategory.value}`
+          )
+          .then((response) => {
+            list.value = response.data.products;
+          })
+          .catch((error) => {
+            console.error("Error fetching products:", error);
+            // products.value = [];
+          });
+      }
+    };
     onMounted(async () => {
       try {
         const response = await axios.get("https://dummyjson.com/products");
@@ -63,69 +113,75 @@ export default {
       } catch (error) {
         console.error(error);
       }
+      fetchCategories();
+    });
+    watch(selectedCategory, () => {
+      fetchProducts();
     });
 
     return {
       list,
-      item_detail
+      item_detail,
+      categories,
+      selectedCategory,
+      fetchProducts,
     };
-  }
+  },
 };
 </script>
 
 <style scoped>
- .mb-4 {
-    margin-bottom: 1.5rem;
-  }
-
-  /* ... your existing styles ... */
-
-  @media (max-width: 768px) {
-    .row.justify-content-center {
-      justify-content: flex-start; /* Adjust alignment for small screens */
-    }
-  }
-  @media screen and (min-width: 1200px) and (max-width: 1500px) {
-    div#landing-cards{
-      width: 25%;
-    }
-  }
-  @media screen and (min-width: 992px) and (max-width: 1199px) {
-    div#landing-cards{
-      width: 33%;
-    }
-    div#category-btns{
-      flex-wrap: wrap;
-    }
-    div#category-btns button {
-      margin-top: 10px;
-    }
-  }
-  @media screen and (max-width: 991px){
-    div#landing-cards{
-      width: 49% !important;
-    }
-  }
-  @media screen and (max-width: 767px) {
-    div#category-btns{
-      flex-wrap: wrap;
-    }
-    div#category-btns button {
-      margin-top: 10px;
-    }
-  }
-  @media screen and (max-width: 576px){
-    div#landing-cards{
-      width: 90% !important;
-    margin: auto;
-    }
-  }
-
-  .card{
-    cursor: pointer;
-  }
-  div#landing-cards {
-    width: 25%;
+.mb-4 {
+  margin-bottom: 1.5rem;
 }
 
+/* ... your existing styles ... */
+
+@media (max-width: 768px) {
+  .row.justify-content-center {
+    justify-content: flex-start; /* Adjust alignment for small screens */
+  }
+}
+@media screen and (min-width: 1200px) and (max-width: 1500px) {
+  div#landing-cards {
+    width: 25%;
+  }
+}
+@media screen and (min-width: 992px) and (max-width: 1199px) {
+  div#landing-cards {
+    width: 33%;
+  }
+  div#category-btns {
+    flex-wrap: wrap;
+  }
+  div#category-btns button {
+    margin-top: 10px;
+  }
+}
+@media screen and (max-width: 991px) {
+  div#landing-cards {
+    width: 49% !important;
+  }
+}
+@media screen and (max-width: 767px) {
+  div#category-btns {
+    flex-wrap: wrap;
+  }
+  div#category-btns button {
+    margin-top: 10px;
+  }
+}
+@media screen and (max-width: 576px) {
+  div#landing-cards {
+    width: 90% !important;
+    margin: auto;
+  }
+}
+
+.card {
+  cursor: pointer;
+}
+div#landing-cards {
+  width: 25%;
+}
 </style>
