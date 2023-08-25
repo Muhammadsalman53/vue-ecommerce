93b301ac4caa5b0be4b12694d80dee8b683ab440
muhammad salman <111515109+Muhammadsalman53@users.noreply.github.com>
2023-08-25 09:24:08 +0500

    Improve product detail

--- vue-ecommerce/src/components/Product-Detail.vue
+++ vue-ecommerce/src/components/Product-Detail.vue
@@ -1,42 +1,43 @@
 <script>
+import { ref, onMounted } from "vue";
 import axios from "axios";
-import { onMounted } from "vue";
-import { ref } from "vue";
+import { useRoute } from "vue-router";
+
 export default {
-    
+    name: "ProductDetail",
     setup() {
-        const list = ref([])
-
+        const route = useRoute();
+        const productId = route.params.id;
+        const product = ref(null);
         onMounted(() => {
-            axios.get("https://dummyjson.com/products")
-            .then(res => {
-                // console.log(res);
-                list.value = res.data.products
-                console.log(list.value);
-
-            }).catch(err => {
-                console.log(err);
-            })
-        })
+            axios
+                .get(`https://dummyjson.com/products/${productId}`)
+                .then((res) => {
+                    product.value = res.data;
+                })
+                .catch((err) => {
+                    console.error(err);
+                });
+        });
 
         return {
-            list
-        }
-    }
+            product,
+        };
+    },
 };
 </script>
 <template>
-    <div v-for="item in list" v-bind:key="item.id" class="container mt-5 card">
+    <div v-if="product" class="container mt-5 card">
         <div class="row">
             <div class="col-md-6 order-lg-0 order-md-0">
                 <div class="product-image">
-                    <img :src="item.images[0]" alt="Product Image" class="img-fluid card-img">
+                    <img :src="product.images[0]" alt="Product Image" class="img-fluid card-img size-img">
                 </div>
             </div>
             <div class="col-md-6 order-lg-1 pt-lg-3 order-md-1 card-text-center">
-                <h1>{{ item.title }}</h1>
-                <h2 class="mb-3">{{ item.price }}</h2>
-                <p>{{ item.description }}</p>
+                <h1>{{ product.title }}</h1>
+                <h2 class="mb-3">{{ product.price }}</h2>
+                <p>{{ product.description }}</p>
                 <div class="mb-3">
                     <label for="quantity" class="form-label">Quantity</label>
                     <input type="number" id="quantity" class="form-control" value="1" min="1">
@@ -48,12 +49,18 @@ export default {
             </div>
         </div>
     </div>
+    <div v-else>Loading...</div>
 </template>
-  
-  
-  
+
+
+
 <style scoped>
 /* Add custom styling if needed */
+.size-img {
+    width: 100%;
+    height: 300px;
+}
+
 @media (max-width: 767px) {
     .product-image {
         text-align: center;
@@ -61,8 +68,6 @@ export default {
 
     .card-text-center {
         text-align: center;
-
     }
 }
 </style>
-  
