b4351ee6d50625fbd766dbd06ec573be1acc1d68
Mz-Bug <111413327+Mz-Bug@users.noreply.github.com>
2023-08-28 04:46:30 -0700

    Cart modification

--- vue-ecommerce/src/stores/counter.js
+++ vue-ecommerce/src/stores/counter.js
@@ -1,17 +1,3 @@
-// import { ref, computed } from 'vue'
-// import { defineStore } from 'pinia'
-
-// export const useCounterStore = defineStore('counter', () => {
-//   const count = ref(0)
-//   const doubleCount = computed(() => count.value * 2)
-//   function increment() {
-//     count.value++
-//   }
-
-//   return { count, doubleCount, increment }
-// })
-// store.js
-// store.js
 import { defineStore } from "pinia";
 
 export const useMyStore = defineStore("myStore", {
--- vue-ecommerce/src/views/cart.vue
+++ vue-ecommerce/src/views/cart.vue
@@ -1,107 +1,136 @@
 <template>
-    <main>
-        <Header />
-        <div class="container p-5">
-            <div class="cart-items" v-if="cartItems.length">
-                <div class="cart-item" v-for="item in cartItems" :key="item.id">
-                    <div class="item-details">
-                        <img :src="item.images[0]" alt="" />
-                        <span>
-                            <h5>Name:</h5>{{ item.title }}
-                        </span>
-                        <span>
-                            <h5>Price:</h5>${{ item.price }}
-                        </span>
-                        <div class="form-outline" style="width: 4rem;">
-                            <i class="fas fa-dollar-sign trailing"></i>
-                            <input type="number" v-model="quantity" id="form1" class="form-control form-icon-trailing"/>
-                        </div>
-                        <button @click="removeFromCart(item)" class="btn btn-danger">Remove</button>
-                    </div>
-                </div>
+  <main>
+    <Header />
+    <div class="container p-5">
+      <div class="d-flex justify-content-start" id="back-btn">
+        <button class="text" @click="$router.push('/home')">
+          <i class="bi bi-arrow-left"></i>
+        </button>
+      </div>
+      <div class="cart-items" v-if="cartItems.length">
+        <div class="cart-item" v-for="item in cartItems" :key="item.id">
+          <div class="item-details">
+            <img :src="item.images[0]" alt="" />
+            <span>
+              <h5>Name:</h5>
+              {{ item.title }}
+            </span>
+            <span>
+              <h5>Price:</h5>
+              ${{ item.price }}
+            </span>
+            <div class="form-outline" style="width: 4rem">
+              <i class="fas fa-dollar-sign trailing"></i>
+              <input
+                type="number"
+                v-model="quantity"
+                id="form1"
+                class="form-control form-icon-trailing"
+              />
             </div>
-            <div v-else>
-                <p>Your cart is empty.</p>
-            </div>
+            <button @click="removeFromCart(item)" class="btn btn-danger">
+              Remove
+            </button>
+          </div>
         </div>
-        <button class="btn btn-success" id="cart-buy-btn" @click="$router.push('/buynow')">Buy Now</button>
-    </main>
+      </div>
+      <div v-else>
+        <p>Your cart is empty.</p>
+      </div>
+    </div>
+    <button
+      class="btn btn-success"
+      id="cart-buy-btn"
+      @click="$router.push('/buynow')"
+    >
+      Buy Now
+    </button>
+  </main>
 </template>
-  
+
 <script>
 import { defineComponent, computed } from "vue";
 import { ref, onMounted } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import axiosInstance from "../utils/axios";
 import { useMyStore } from "../stores/counter.js";
-import Header from '../components/Header.vue';
+import Header from "../components/Header.vue";
 
 export default defineComponent({
-    name: "CartView",
-    components: {
-        Header,
-    },
-    data() {
-        return {
-            quantity: 1,
-        };
-    },
-    setup() {
-        const router = useRouter();
-        const route = useRoute();
-        const productId = route.params.id;
-        const store = useMyStore();
+  name: "CartView",
+  components: {
+    Header,
+  },
+  data() {
+    return {
+      quantity: 1,
+    };
+  },
+  setup() {
+    const router = useRouter();
+    const route = useRoute();
+    const productId = route.params.id;
+    const store = useMyStore();
 
-        const addToCart = (item) => {
-            store.commit("addToCart", item);
-        };
-        const removeFromCart = (item) => {
-            store.removeFromCart(item)
-        };
+    const addToCart = (item) => {
+      store.commit("addToCart", item);
+    };
+    const removeFromCart = (item) => {
+      store.removeFromCart(item);
+    };
 
-        const cartItems = computed(() => store.cartItems);
+    const cartItems = computed(() => store.cartItems);
 
-        onMounted(() => {
-            axiosInstance
-                .get(`/products/${productId}`)
-                .then((res) => {
-                    cartItems.value.push(res.data);
-                    console.log(cartItems.value);
-                })
-                .catch((err) => {
-                    console.error(err);
-                });
+    onMounted(() => {
+      axiosInstance
+        .get(`/products/${productId}`)
+        .then((res) => {
+          cartItems.value.push(res.data);
+          console.log(cartItems.value);
+        })
+        .catch((err) => {
+          console.error(err);
         });
+    });
 
-        return {
-            store,
-            addToCart,
-            removeFromCart,
-            cartItems,
-        };
-    },
+    return {
+      store,
+      addToCart,
+      removeFromCart,
+      cartItems,
+    };
+  },
 });
 </script>
-  
+
 <style scoped>
 .item-details {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 32px;
-    box-shadow: 0 0 17px 6px #e7e7e7;
-    border-radius: 8px;
-    padding: 16px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 32px;
+  box-shadow: 0 0 17px 6px #e7e7e7;
+  border-radius: 8px;
+  padding: 16px;
 }
 
 .item-details img {
-    width: 20%;
-    height: 20%;
+  width: 20%;
+  height: 20%;
 }
-#cart-buy-btn{
-    position: fixed;
-    bottom: 5%;
-    right: 3%;
+#cart-buy-btn {
+  position: fixed;
+  bottom: 5%;
+  right: 3%;
+}
+#back-btn {
+  padding: 12px 0;
+}
+#back-btn button {
+  background-color: #fff;
+  border: none;
+}
+#back-btn button i {
+  font-size: 2rem;
 }
 </style>
-  
